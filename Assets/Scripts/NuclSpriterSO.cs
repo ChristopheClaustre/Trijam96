45967a6df297195964e05db66f78ab335fa99b2b
ChristopheClaustre <christophe.claustre.31@gmail.com>
2020-11-30 23:51:42 +0100

    [FCT] Container and manager for the different versions of Nucl sprites

---
+++ Assets/Scripts/NuclSpriterSO.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NuclSpriter", menuName = "Nucl Spriter")]
+public class NuclSpriterSO : ScriptableObject
+{
+    public List<Sprite> UISprites;
+    public List<Sprite> CombSprites;
+    public List<Sprite> GeneratedSprites;
+    
+    public Sprite GetUISprite(NuclManager.NuclEnum index)
+    {
+        try
+        {
+            return UISprites[(int)index];
+        }
+        catch (System.Exception)
+        {
+            return null;
+        }
+    }
+
+    public Sprite GetCombSprite(NuclManager.NuclEnum index)
+    {
+        try
+        {
+            return CombSprites[(int)index];
+        }
+        catch (System.Exception)
+        {
+            return null;
+        }
+    }
+
+    public Sprite GetGeneratedSprite(NuclManager.NuclEnum index)
+    {
+        try
+        {
+            return GeneratedSprites[(int)index];
+        }
+        catch (System.Exception)
+        {
+            return null;
+        }
+    }
+}
