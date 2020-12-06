826db58abdf68e72304d5db394064420d30d1ea5
ChristopheClaustre <christophe.claustre.31@gmail.com>
2020-12-06 14:40:33 +0100

    [FCT] OpenURL: now it opens in a new window on WebGL

--- Assets/Scripts/UI/OpenURL.cs
+++ Assets/Scripts/UI/OpenURL.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.InteropServices;
 
 public class OpenURL : MonoBehaviour
 {
     public void Open(string url)
     {
-        Application.OpenURL(url);
+        #if (UNITY_WEBGL && !UNITY_EDITOR)
+            openWindow(url);
+        #else
+            Application.OpenURL(url);
+        #endif
     }
+
+#if UNITY_WEBGL
+    [DllImport("__Internal")]
+	private static extern void openWindow(string url);
+#endif
 }
